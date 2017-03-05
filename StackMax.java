bfc51b5da52733893c8f1d922eba321c60447781
sagar shahi <shahisagar20@gmail.com>
2017-03-05 06:33:04 -0800

    changes at the popMax method

--- StackMax.java
+++ StackMax.java
@@ -1,16 +1,23 @@
 package assignment2;
-/*
+/*Sagar Shahi
+ * Assignment2Q#2
+ * 
  * This program will find the maximum value in the stack
- * And it will not pop the value out of the stack
+ * And it will not pop the value out of the stack if you just want to see the max value in the stack
+ * If you want to pop the maximum value in the stack, it will pop the maximum value
+ * and restore the stack as it should have been in after max-value is removed from the stack.
+ * I used 3 stacks
+ * One stack to behave as a regular stack, another stack to keep track of the maximum value, and 
+ * another re-usable temporary stack.
  */
 
 import java.util.Stack;
 
 public class StackMax {
 
-	Stack<Integer> stack = new Stack();
-	Stack<Integer> maxStack = new Stack();
-	Stack<Integer> tempStack = new Stack();
+	private Stack<Integer> stack = new Stack(); // initialize the three stacks
+	private Stack<Integer> maxStack = new Stack();
+	private Stack<Integer> tempStack = new Stack();
 	/*
 	 * This will add the given item into the stack
 	 * 
@@ -23,16 +30,21 @@ public class StackMax {
 			maxStack.push(item);
 		} else {
 			stack.add(item); // adds the item into the stack
-			int max = maxStack.peek();
-			while (item < max && !maxStack.isEmpty()) {
-				tempStack.push(maxStack.pop());
-				if (maxStack.isEmpty()) {
-					maxStack.push(item);
+			int max = maxStack.peek();// stores the value in maxStack
+			while (item < max && !maxStack.isEmpty()) {// runs a loop to sort
+														// the stack and
+				tempStack.push(maxStack.pop());// puts the value with largest
+												// values from top to smallest
+												// bottom
+				if (maxStack.isEmpty()) {// checks if there no move items left
+											// in the stack for comparison
+					maxStack.push(item);// then puts the smallest value in the
+										// stack
 				} else {
 					max = maxStack.peek();
 				}
 			}
-			maxStack.push(item);
+			maxStack.push(item);// restores the maxStack
 			while (!tempStack.isEmpty()) {
 				maxStack.push(tempStack.pop());
 			}
@@ -41,13 +53,14 @@ public class StackMax {
 
 	/*
 	 * This method will remove the latest item added into the stack
+	 * @return Returns the latest value added to the stack.
 	 */
 	public int pop() {
-		if (!stack.isEmpty() && !maxStack.isEmpty()) {
+		if (!stack.isEmpty()) {
 			int max = maxStack.peek(); // looks at the first item in the
 										// maxStack
-			int myItem = stack.pop();
-			while (max != myItem && !maxStack.isEmpty()) {
+			int myItem = stack.pop();//pops the first item from the stack
+			while (max != myItem && !maxStack.isEmpty()) { //
 				tempStack.push(maxStack.pop());
 				max = maxStack.peek();
 			}
@@ -70,7 +83,7 @@ public class StackMax {
 	}
 
 	public int popMax() {
-		if (!maxStack.isEmpty() && !!stack.isEmpty()) {
+		if (!maxStack.isEmpty()) {
 			int max = maxStack.pop();
 			int myValue = stack.peek();
 			while (myValue != max) {
@@ -82,21 +95,28 @@ public class StackMax {
 				stack.push(tempStack.pop());
 			}
 			return retValue;
-		}else{
+		} else {
 			return 0;
 		}
 	}
 
 	public static void main(String[] args) {
 		StackMax max = new StackMax();
+		max.push(5);
+		max.push(6);
+		max.push(7);
+		max.push(8);
+		max.push(9);
 		max.push(100);
 		max.push(11);
 		max.push(13);
-		System.out.println(max.popMax());
-		System.out.println(max.popMax());
-		System.out.println(max.popMax());
-		System.out.println(max.popMax());
-		System.out.println(max.getMax());
+
+		int x = 8;
+		while (x > 0) {
+			System.out.println("rerPop: " + max.pop());
+			System.out.println("maxPop: " + max.popMax());
+			x--;
+		}
 	}
 
 }
